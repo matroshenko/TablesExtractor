3ecf64e04bc1380bc6437de6fd268b4cc4cb5230
Mikhail Atroshchenko <m.atroshenko@gmail.com>
2022-03-28 14:39:41 +0000

    Добавил юнит-тест.

---
+++ tests/test_interval.py
@@ -0,0 +1,23 @@
+from unittest import TestCase, main
+
+import context
+from interval import Interval, sort_and_merge_intervals
+
+
+class IntervalTestCase(TestCase):
+    def test_merge_intervals(self):
+        intervals = [
+            Interval(0, 4),
+            Interval(6, 9),
+            Interval(2, 5),
+            Interval(7, 8),
+            Interval(1, 3)
+        ]
+        result = sort_and_merge_intervals(intervals)
+        expected_result = [Interval(0, 5), Interval(6, 9)]
+        
+        self.assertListEqual(result, expected_result)
+
+
+if __name__ == '__main__':
+    main()
