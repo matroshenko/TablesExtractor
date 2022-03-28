da89ccd771fae07e9576abf1f781055ab92c827f
Mikhail Atroshchenko <m.atroshenko@gmail.com>
2022-03-28 10:38:00 +0000

    Реализовал метод extract.

--- tables_extractor.py
+++ tables_extractor.py
@@ -1,3 +1,7 @@
+from tables_finder import TablesFinder
+from table_analyzer import TableAnalyzer
+from cell_text_recognizer import CellTextRecognizer
+
 
 class Cell(object):
     def __init__(self, grid_rect, recognized_text):
@@ -16,4 +20,20 @@ class TablesExtractor(object):
         self._page_objects = page_objects
 
     def extract(self):
-        pass
+        tables_finder = TablesFinder(self._page_objects)
+        rects = tables_finder.find()
+
+        tables = []
+        for rect in rects:
+            table_analyzer = TableAnalyzer(self._page_objects, rect)
+            grid_structure, cells_grid_rects = table_analyzer.analyze()
+            cells = []
+            for cell_grid_rect in cells_grid_rects:
+                cell_rect = grid_structure.get_cell_rect(cell_grid_rect)
+                cell_text_recognizer = CellTextRecognizer(self._page_objects, cell_rect)
+                text = cell_text_recognizer.recognize()
+                cells.append(Cell(cell_grid_rect, text))
+
+            tables.append(Table(grid_structure, cells))  
+                      
+        return tables
