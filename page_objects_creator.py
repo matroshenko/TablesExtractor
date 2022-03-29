a7eb709cb525ba4e3baf727bf5fef12046452ee8
Mikhail Atroshchenko <m.atroshenko@gmail.com>
2022-03-29 07:26:56 +0000

    Реализовал механизм создания объектов страницы.

---
+++ page_objects_creator.py
@@ -0,0 +1,36 @@
+import fitz
+import PIL
+
+from page_objects import Word, PageObjects
+from rect import Rect
+
+
+class PageObjectsCreator(object):
+    def create(self, pdf_file_path):
+        """Returns list of PageObjects. Length of list is equal to pages count."""
+        
+        doc = fitz.open(pdf_file_path)
+        result = []
+        for page in doc:
+            result.append(
+                PageObjects(self.get_page_image(page), self.get_words(page))
+            )
+            
+        return result
+
+    def get_page_image(self, page):
+        pixmap = page.get_pixmap(dpi=72)
+        size = (pixmap.width, pixmap.height)
+        return PIL.Image.frombuffer('RGB', size, pixmap.samples_mv)
+
+    def get_words(self, page):
+        text_page = page.get_textpage()
+        words = text_page.extractWORDS()
+
+        result = []
+        for word in words:
+            left, top, right, bottom, text, *_ = word
+            result.append(Word(Rect(left, top, right, bottom), text))
+
+        return result
+
