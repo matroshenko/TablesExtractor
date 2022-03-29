37d3817aa52f7e84a2f27e6c7d2f2926efebfd25
Mikhail Atroshchenko <m.atroshenko@gmail.com>
2022-03-29 08:38:57 +0000

    Добавил юнит-тест.

---
+++ tests/test_tables_extractor.py
@@ -0,0 +1,47 @@
+from unittest import TestCase, main
+
+import context
+from page_objects_creator import PageObjectsCreator
+from tables_extractor import TablesExtractor
+
+
+class TablesExtractorTestCase(TestCase):
+    def test_single_table(self):
+        page_objects_creator = PageObjectsCreator()
+        page_objects_list = page_objects_creator.create('data/page_28.pdf')
+        
+        self.assertEqual(len(page_objects_list), 1)
+        page_objects = page_objects_list[0]
+
+        tables_extractor = TablesExtractor(page_objects)
+        tables = tables_extractor.extract()
+
+        self.assertEqual(len(tables), 1)
+        table = tables[0]
+
+        grid_structure = table.grid_structure
+        self.assertEqual(grid_structure.get_rows_count(), 4)
+        self.assertEqual(grid_structure.get_cols_count(), 4)
+
+        cells = table.cells
+        self.assertEqual(len(cells), 16)
+
+        self.assertEqual(cells[0].text, '')
+        self.assertEqual(cells[1].text, '2017')
+        self.assertEqual(cells[2].text, '2016')
+        self.assertEqual(cells[3].text, '2015')
+
+        self.assertEqual(cells[4].text, 'Low')
+        self.assertEqual(cells[5].text, '3.25%')
+        self.assertEqual(cells[6].text, '2.75%')
+        self.assertEqual(cells[7].text, '4.50%')
+
+        self.assertEqual(cells[8].text, 'High')
+        self.assertEqual(cells[9].text, '4.50')
+        self.assertEqual(cells[10].text, '4.50')
+        self.assertEqual(cells[11].text, '7.00')
+
+        self.assertEqual(cells[12].text, 'Weighted-average')
+        self.assertEqual(cells[13].text, '4.03')
+        self.assertEqual(cells[14].text, '3.82')
+        self.assertEqual(cells[15].text, '5.90')
