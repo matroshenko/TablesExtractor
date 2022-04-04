41c7eeb4911d649258a59b8e4c4aa8dc3f0dd21d
Mikhail Atroshchenko <m.atroshenko@gmail.com>
2022-04-04 09:53:51 +0000

    Исправил баг.

--- table_to_html_exporter.py
+++ table_to_html_exporter.py
@@ -2,14 +2,14 @@ import xml.etree.ElementTree as ET
 
 
 class TableToHTMLExporter(object):
-    def __init__(self, table):
-        self._table = table
+    def __init__(self, table_cells):
+        self._table_cells = table_cells
 
     def export(self):
         table_element = ET.Element('table')
         row_element = ET.SubElement(table_element, 'tr')
 
-        cells = self._table.cells
+        cells = self._table_cells
         current_row = cells[0].grid_rect.top
         for cell in cells:
             if cell.grid_rect.top > current_row:
@@ -20,9 +20,9 @@ class TableToHTMLExporter(object):
             colspan = cell.grid_rect.get_width()
             cell_element = ET.SubElement(row_element, 'td')
             if rowspan > 1:
-                cell_element['rowspan'] = rowspan
+                cell_element.set('rowspan', str(rowspan))
             if colspan > 1:
-                cell_element['colspan'] = colspan
+                cell_element.set('colspan', str(colspan))
             cell_element.text = cell.text
         
         return table_element
