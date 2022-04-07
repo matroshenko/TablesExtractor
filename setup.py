ae9a4918e032fbfaf59561511a2f7964b46c583b
Mikhail Atroshchenko <m.atroshenko@gmail.com>
2022-04-07 12:19:58 +0000

    Устанавливаем модели.

--- setup.py
+++ setup.py
@@ -1,9 +1,20 @@
 from setuptools import setup
+import os
+
+def package_files(directory):
+    paths = []
+    for (path, directories, filenames) in os.walk(directory):
+        for filename in filenames:
+            paths.append(os.path.join('..', path, filename))
+    return paths
+
+extra_files = package_files('tables_extractor/models')
 
 setup(
     name='TablesExtractor',
     version='0.1',
     packages=['tables_extractor'],
+    package_data={'tables_extractor': extra_files},
     install_requires=[
         'Pillow==9.0.1',
         'tensorflow==2.8.0',
