58989aaf5842a07ac9c815f37615d9aa35f06a59
Mikhail Atroshchenko <m.atroshenko@gmail.com>
2022-04-07 09:29:44 +0000

    Отключаем warning'и tensorflow.

--- tests/test_tables_extractor.py
+++ tests/test_tables_extractor.py
@@ -4,6 +4,9 @@ import os
 # Turn off tensorflow warnings.
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
 
+import tensorflow as tf
+tf.get_logger().setLevel('ERROR')
+
 import context
 from tables_extractor.page_objects_creator import PageObjectsCreator
 from tables_extractor.tables_extractor import TablesExtractor
